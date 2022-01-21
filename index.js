d57514a0f1345d772f0da6443c8fc1c7e8e38ce0
KatrinSH <93479330+KatrinSH@users.noreply.github.com>
2022-01-21 16:47:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import "./styles.css";
+import Swal from 'sweetalert2'
+var button = document.querySelector("button");
 
+button.addEventListener("click", function () {
+  myFunk();
+});
+
+function myFunk() {
+  Swal.fire(
+    'The Internet?',
+    'That thing is still around?',
+    'question'
+  );
+}
